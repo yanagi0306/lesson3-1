56f6cfa1d5f2c8533045b33b9836c71167ea7def
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-04-28 12:20:29 +0900

    課題3−1

--- main.js
+++ main.js
@@ -2,21 +2,16 @@
 {
   const todos = [];
 
-  document.getElementById(`add`).addEventListener(`click`, () => {
-    const todo = {
-      task: document.getElementById(`add-text`).value,
-      status: `作業中`,
-    }
-    todos.push(todo);
-
+  const listRiset = () => {
     const lists = document.querySelectorAll(`tr`)
     lists.forEach(function (list, i) {
       if (i > 0) {
         list.remove();
       }
     });
+  }
 
-
+  const addList = () => {
     for (let i = 0; i < todos.length; i++) {
       const tr = document.createElement(`tr`);
       document.querySelector(`table`).appendChild(tr);
@@ -29,25 +24,44 @@
       coment.textContent = todos[i].task;
       tr.appendChild(coment);
 
+      const workBtntd = document.createElement(`td`)
       const workBtn = document.createElement(`input`)
       workBtn.value = todos[i].status;
       workBtn.type = `button`;
-      workBtn.classList.add(`btn`);
-      tr.appendChild(workBtn);
+      workBtn.classList.toggle(`btn`);
+      workBtn.classList.toggle(`sta`);
+      tr.appendChild(workBtntd);
+      workBtntd.appendChild(workBtn);
 
+
+      const delBtntd = document.createElement(`td`);
       const delBtn = document.createElement(`input`);
       delBtn.value = `削除`;
       delBtn.type = `button`;
-      delBtn.classList.add(`btn`);
-      tr.appendChild(delBtn);
+      delBtn.classList.toggle(`btn`);
+      delBtn.classList.toggle(`del`);
+      tr.appendChild(delBtntd);
+      delBtntd.appendChild(delBtn);
+      delBtn.addEventListener(`click`, () => {
+        todos.splice(i, 1);
+        listRiset();
+        addList();
+      });
     }
-    document.getElementById(`add-text`).value = ""
+  }
+  
+  document.getElementById(`add`).addEventListener(`click`, () => {
+    const todo = {
+      task: document.getElementById(`add-text`).value,
+      status: `作業中`,
+    }
+    todos.push(todo);
+    listRiset();
+    addList();
+    document.getElementById(`add-text`).value = ``
   });
+
 }
-
-
-
-
 
 
 
